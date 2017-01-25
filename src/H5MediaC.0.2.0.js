91a88c271929bcc8ac0c178460286a738ab988ef
songyijian <397032724@qq.com>
2017-01-25 19:51:58 +0800

    2

--- src/H5MediaC.0.2.0.js
+++ src/H5MediaC.0.2.0.js
@@ -7,7 +7,7 @@
 		obj	//* string || dom   如果是 .MP3||.MP4 的路径（src）将自动创建媒体元素 （插入位置 见下面配置）  
 		
 		new H5MediaC(obj,{
-			'attr': {'controls':'','autoplay':'','loop':''},// json 属性设置 如果传入的不是dom对象src其实也可以写在里面
+			'attr': {"preload":'auto','controls':'','autoplay':'','loop':''},// json 属性设置 value ===falsh视为删除该属性； 注意默认有preload ；
 			'elAppend':document.body , 		//dom 创建的媒体对象插入位置 ，  默认document.body  如果为false不插入
 			'initFn': function(_this){},	//init回调
 			'pausedFn':function(_this){},	//暂停回调
@@ -23,11 +23,10 @@
 			.remov() 干掉创建的元素
 			
 		Attr
-			data 所有配置至高无上
+			data 配置权重最高
 			
 			this.currentTime;	 播放进度
 			this.duration;		视频总时长(这个值当元素加载到页面上才会获取)
-			
 			this.state; 		播放状态 （播放时为true）
 			this.paused; 		暂停状态 （暂停时为true） 【是不是觉得有点脑残，为了照顾原生api习惯 多多益善】
 			
@@ -45,7 +44,7 @@
 		this.obj = obj;
 		if(arguments.length === 1) ajson={};
 		this.data = {
-			'attr': {"preload":'auto' /*'controls': '','autoplay': '','loop': ''*/},
+			'attr': { /*"preload":'auto','controls': '','autoplay': '','loop': ''*/},
 			'currentTime': 0,
 			'elAppend':function(){
 				if(!ajson.elAppend ){
@@ -121,11 +120,16 @@
 		//Event
 		var _this = this;
 		this.el.addEventListener('play', function() {
+			_this.paused = _this.el.paused;
+			_this.state = !_this.el.paused;
 			_this._play()
+			_this.data.toggleFn(_this);
 		})
 		this.el.addEventListener('pause', function() {
+			_this.paused = _this.el.paused;
+			_this.state = !_this.el.paused;
 			_this.data.pausedFn(_this)
-			_this.state = _this.paused = _this.el.paused;
+			_this.data.toggleFn(_this);
 		})
 		
 		this.el.addEventListener('ended',function(){
@@ -140,14 +144,11 @@
 		})
 		
 		
-		
-		
 		//当媒介长度改变时运行的脚本
 		this.el.ondurationchange=function(){ _this.duration = _this.el.duration; }	//1
 		this.el.onloadeddata=function(){}	//2
-	
-		_this.state = !_this.el.paused;
-		_this.paused = _this.el.paused;
+		
+		return this;
 	};
 	
 	H5MediaC.prototype.setData = function(newdata,currentTime,fn) {
@@ -156,7 +157,6 @@
 				this.data.attr[i] = newdata[i]
 			}
 
-			
 			var key = '';
 			for(var i in this.data.attr) {
 				if(this.data.attr.hasOwnProperty(i)) key = i;
@@ -192,6 +192,8 @@
 		}
 		
 		this.data.setDataFn(this)
+		
+		return this;
 	}
 	
 	
@@ -204,8 +206,6 @@
 				_this.currentTime = _this.el.currentTime;
 				_this.data.playFn(_this)
 			}
-			_this.state = !_this.el.paused;
-			_this.paused = _this.el.paused;
 			if(_this.state) { requestAnimationFrame(getTime);}
 		})()
 	};
@@ -217,15 +217,17 @@
 		} else {
 			this.el.pause();
 		}
-		this.data.toggleFn(this.state);
+		return this;
 	}
 	
 	H5MediaC.prototype.play = function(newdata) {
 		this.el.play()
+		return this;
 	}
 	
 	H5MediaC.prototype.pause = function(newdata) {
 		this.el.pause()
+		return this;
 	}
 	
 	//不完美待处理
@@ -237,6 +239,7 @@
         	this.fullScreenFlag = true;
             this.el.webkitRequestFullscreen();
         }
+        return this;
     }
 	
 	//非常不完美
@@ -245,10 +248,10 @@
 			this.data.elAppend.removeChild(this.el);
 			this.el=null
 		}
+		return this;
 	}
 
 	window.H5MediaC = H5MediaC;
-
 }()
 if (typeof(module) !== 'undefined'){
     module.exports = window.H5MediaC;
@@ -258,4 +261,3 @@ if (typeof(module) !== 'undefined'){
         return window.H5MediaC;
     });
 }
-
